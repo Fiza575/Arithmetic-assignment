96cd115e728b3dfad932dd007ac6a8661f5c1a22
Fiza Arzoo Khalid <shaizakanwal025@gmail.com>
2025-05-11 19:25:23 +0500

    Add files via upload

---
+++ Arithemetic_assignment.py
@@ -0,0 +1,58 @@
+# Task no 1 (Addition)
+
+x = 5
+y = 7
+result = x+y
+print("Addition,","The sum of",x,'and',y,'is',result)
+
+# Task no 2 (Subtraction)
+
+y = 7
+x = 5
+result = y-x
+print("Subtraction,","The difference of",x,"and",y,"is",result)
+
+# Task no 3 (Multiplication)
+
+x = 5
+y = 7
+result = x*y
+print("Multiplication,","The  result of",x,"and",y,"is",result)
+
+# Task no 4 (Division)
+
+x = 5
+y = 7
+result =x/y
+print("Division,","The result of",x,"and",y,"is",result)
+
+# Task no 5 (Floor Division)
+
+x = 5
+y = 7
+result =x//y
+print("Floor Division,","The result of",x,"and",y,"is",result)
+
+# Task no 6 (Modulus)
+
+x = 5
+y = 7
+result = x%y
+print("Modulus,","The result of",x,"and",y,"is",result)
+
+# Task no 7 (Exponentiation)
+
+x = 5
+y = 7
+result =x**y
+print("Exponentiation,","The result of",x,"and",y,"is",result)
+
+# Task no 8 (All in one Table)
+
+print(f"{y}+{x}={y+x}")
+print(f"{y}-{x}={y-x}")
+print(f"{y}*{x}={y*x}")
+print(f"{y}/{x}={y/x}")
+print(f"{y}//{x}={y//x}")
+print(f"{y}%{x}={y%x}")
+print(f"{y}**{x}={y**x}")
